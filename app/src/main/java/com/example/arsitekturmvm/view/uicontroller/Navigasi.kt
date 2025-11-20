a9c19da2b91b2e279846dfe044869b06355a4a7f
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-11-20 17:25:45 +0700

    menambahkan enum navigasi

---
+++ app/src/main/java/com/example/arsitekturmvm/view/uicontroller/Navigasi.kt
@@ -0,0 +1,23 @@
+package com.example.arsitekturmvm.view.uicontroller
+
+import com.example.arsitekturmvm.view.FormSiswa
+import com.example.arsitekturmvm.view.TampilData
+import com.example.arsitekturmvm.viewmodel.SiswaViewModel
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.arsitekturmvm.model.DataJK
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
